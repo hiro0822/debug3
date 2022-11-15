09fd3fa90a43f07f2e456651c0fe2d8878fae4ed
hiro0822 <ishi0822hiro@outlook.jp>
2022-11-15 08:23:23 +0000

    コントローラー作成

---
+++ app/models/book.rb
@@ -0,0 +1,33 @@
+class Book < ApplicationRecord
+   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments,dependent: :destroy
+
+   has_one_attached :image
+
+
+
+  validates :title,presence:true
+  validates :body,presence:true
+  validates :body,length:{maximum:200}
+
+  def self.looks(search,word)
+    if search == "perfect_match"
+        @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+        @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+        @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+        @book = Book.where("title LIKE?","%#{word}%")
+    else
+        @book = Book.all
+    end
+  end
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root to:"homes#top"
+
+  get "home/about"=>"homes#about"
+  get "search"=>"searches#search"
+  devise_for :users
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
+  resource  :favorites, only: [:create,:destroy]
+  resources :book_comments,only: [:create,:destroy]
+
+  end
+
+  resources :users, only: [:index,:show,:edit,:update] do
+  resource :relationships, only: [:create,:destroy] do
+  member do
+
+  get :followings,:followers
+
+  end
+  end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
